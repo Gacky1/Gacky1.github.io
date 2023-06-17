42f77109a97314a71e88b91588cce5d9d1ab5637
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2023-06-17 14:59:17 +0530

    All files are deleted and site is updated

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("Script running...")
-document.querySelector('.cross').style.display='none';
-document.querySelector('.hamburger').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo');
-})
